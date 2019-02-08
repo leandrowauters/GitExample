d4ee60d2b36b55580ad5882395dcd7107770e9a1
John Doe <leandro.wauters@gmail.com>
2019-02-08 10:55:06 -0500

    first commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Leandro Wauters on 2/8/19.
+//  Copyright © 2019 Leandro Wauters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("working")
+    }
+
+
+}
+
